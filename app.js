7e55420b7d2b45a8d2b615384c530833cd7a4d1e
BuhleMtshali <mtshalizanobuhle7@gmail.com>
2024-09-13 21:33:57 +0200

    updated the js file

---
+++ app.js
@@ -0,0 +1,7 @@
+let inputDayElement = document.getElementById("day");
+let inputMonthElement = document.getElementById("month");
+let inputYearElement = document.getElementById("year");
+let outputYearElement = document.getElementById("output-year");
+let outputMonthElement = document.getElementById("output-month");
+let outputDayElement = document.getElementById("output-days");
+let calculateBtn = document.getElementById("arrow");
