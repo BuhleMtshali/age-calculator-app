4c2cca3ab20c0885a285ec818112040e56a2e371
BuhleMtshali <mtshalizanobuhle7@gmail.com>
2024-09-13 21:43:24 +0200

    updated the app.js file

--- app.js
+++ app.js
@@ -5,3 +5,14 @@ let outputYearElement = document.getElementById("output-year");
 let outputMonthElement = document.getElementById("output-month");
 let outputDayElement = document.getElementById("output-days");
 let calculateBtn = document.getElementById("arrow");
+let date = new Date();
+
+//function
+calculateBtn.addEventListener("click", () => {
+  let currentYear = new Date(inputYearElement.value);
+  let currentMonth = new Date(inputMonthElement.value);
+  let currentDay = new Date(inputDayElement.value);
+
+  let yearDifference = date.getFullYear() - currentYear.getFullYear();
+  outputYearElement.innerText = yearDifference;
+});
