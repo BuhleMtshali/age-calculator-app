4fe9cef4e673f2e08c8e574ddb0449fd8b6ecc1d
BuhleMtshali <mtshalizanobuhle7@gmail.com>
2024-09-15 17:28:14 +0200

    updated js file

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ let outputMonthElement = document.getElementById("output-month");
 let outputDayElement = document.getElementById("output-days");
 let calculateBtn = document.getElementById("arrow");
 let date = new Date();
+let today = new Date();
 
 //function
 calculateBtn.addEventListener("click", () => {
@@ -15,9 +16,14 @@ calculateBtn.addEventListener("click", () => {
   let yearDifference = date.getFullYear() - currentYear.getFullYear();
   let monthDifference = date.getMonth() - currentMonth.getMonth();
   let dayDifference = date.getDate() - currentDay.getDate();
+  let nextBirthday = new Date(currentYear, currentMonth, currentDay);
+  let timeDifference = nextBirthday - today;
+  let daysDifference = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+  //displaying results
   outputYearElement.innerText = yearDifference;
   outputMonthElement.innerText = monthDifference;
-  outputDayElement.innerText = dayDifference;
+  outputDayElement.innerText = dayDifference % 30;
+  //outputDayElement.innerText = dayDifference;
   inputDayElement.value = "";
   inputMonthElement.value = "";
   inputYearElement.value = "";
